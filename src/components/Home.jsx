863b3dc647074e3c0fd072f04f10efda624235a5
NachoArz <nachoarz22@hotmail.com>
2024-11-19 20:55:08 -0300

    dia 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,28 @@
 const Home = () => {
+  const fetchMovies = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`,
+        options
+      );
+      const newMovies = response.data.results;
+      if (newMovies.length === 0) {
+        setHasMore(false);
+      } else {
+        setMovies((prevMovies) => {
+          const uniqueMovies = newMovies.filter(
+            (newMovie) => !prevMovies.some((movie) => movie.id === newMovie.id)
+          );
+          return [...prevMovies, ...uniqueMovies];
+        });
+
+        setPage((prevPage) => prevPage + 1);
+      }
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  };
+
   return (
     <div>
       <nav></nav>
--- src/components/Pelicula.jsx
+++ src/components/Pelicula.jsx
@@ -1,5 +1,22 @@
 const Pelicula = () => {
-  return <div>Pelicula</div>;
+  return (
+    <div>
+      <h1>Titulo: {}</h1>
+      <p>
+        Duracion: {} Año: {} Rating: {} "logo de imdb"
+      </p>
+      <h2>Generos:</h2>
+      <h2>Reparto:</h2>
+      <h2>Directores:</h2>
+      <h2>Descripcion:</h2>
+      <p> {"....."}</p>
+      <div>
+        <button>Añadir al carrito</button>
+        <button>Trailer</button>
+        <button>Logo de compartir</button>
+      </div>
+    </div>
+  );
 };
 
 export default Pelicula;
