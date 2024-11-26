25105e871533dab3e2024e2885b1220760621743
Mauro Gonzalez <maurogonzalezbruno@gmail.com>
2024-11-26 14:23:47 -0300

    add persist config

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
 import authReducer from "../reducer/authSlice"
 import cartReducer from "../reducer/cartSlice"
 
+const persistConfig = {
+  key: "todo",
+  storage,
+}
+
+const rootReducer = combineReducers({ auth: authReducer, cart: cartReducer })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
 
+export const persistor = persistStore(store)
 export default store
