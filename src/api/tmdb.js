fa76a0ea4f718bf6de882c5fefbeb9662d9e2802
Mauro Gonzalez <maurogonzalezbruno@gmail.com>
2024-11-29 06:07:38 -0300

    add generos for movies

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -5,25 +5,67 @@ const config = {
 }
 
 const apiKey = import.meta.env.VITE_API_KEY
-const urlTmdb = "https://api.themoviedb.org/3"
+const urlTmdb = "https://api.themoviedb.org/3/movie"
 
 const fetchMovies = async () => {
   try {
-    const response = await axios({
-      method: "GET",
-      url: `${urlTmdb}/movie/popular`,
-      params: { language: "en-US", page: "1" },
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-    })
-    if (response.status === 200) {
-      return { data: response.data.results, error: null }
+    const [popular, top_rated, now_playing, upcoming] = await axios.all([
+      axios({
+        method: "GET",
+        url: `${urlTmdb}/popular`,
+        params: { language: "en-US", page: "1" },
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }),
+      axios({
+        method: "GET",
+        url: `${urlTmdb}/top_rated`,
+        params: { language: "en-US", page: "1" },
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }),
+      axios({
+        method: "GET",
+        url: `${urlTmdb}/now_playing`,
+        params: { language: "en-US", page: "1" },
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }),
+      axios({
+        method: "GET",
+        url: `${urlTmdb}/upcoming`,
+        params: { language: "en-US", page: "1" },
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }),
+    ])
+    if (
+      popular.status === 200 &&
+      top_rated.status === 200 &&
+      now_playing.status === 200 &&
+      upcoming.status === 200
+    ) {
+      return {
+        data: {
+          popular: popular.data.results,
+          top_rated: top_rated.data.results,
+          now_playing: now_playing.data.results,
+          upcoming: upcoming.data.results,
+        },
+        error: null,
+      }
     }
   } catch (error) {
     if (error.response) {
-      return { error: "Error: algo salio mal", data: null }
+      return { error: "Error: Algo salio mal", data: null }
     }
   }
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import { fetchMovies } from "../api/tmdb"
 import MovieCard from "../components/MovieCard"
 // import { FaShoppingCart } from "react-icons/fa"
 const Home = () => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState({
+    popular: [],
+    top_rated: [],
+    now_playing: [],
+    upcoming: [],
+  })
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
 
@@ -29,10 +34,29 @@ const Home = () => {
 
   return (
     <div className="main-contain">
+      {console.log(movies.popular)}
       <section>
-        <h2 className="cart">Popular-Movie</h2>
+        <h2 className="cart">Peliculas Populares</h2>
         <div className="movie-list">
-          {movies.map(movie => (
+          {movies.popular.map(movie => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+        <h2 className="cart">Peliculas Mejor Calificadas</h2>
+        <div className="movie-list">
+          {movies.top_rated.map(movie => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+        <h2 className="cart">Now Playing</h2>
+        <div className="movie-list">
+          {movies.now_playing.map(movie => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+        <h2 className="cart">Estrenos</h2>
+        <div className="movie-list">
+          {movies.upcoming.map(movie => (
             <MovieCard movie={movie} key={movie.id} />
           ))}
         </div>
