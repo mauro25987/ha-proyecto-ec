b8e20209d0d0eb23801f61ab7b68484563b8940e
NachoArz <nachoarz22@hotmail.com>
2024-12-05 14:18:50 -0300

    responsive actualizado

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -79,58 +79,6 @@ const About = () => {
           </div>
         </div>
       </div>
-      {/* <div className="card-container">
-        <div className="card">
-          
-          <h2>Colaborador destacado</h2>
-          <div className="collaborator-card">
-            <img
-              src="https://i.pinimg.com/originals/01/7e/6f/017e6f2d0c32627056d26a7e77c75753.png"
-              className="collaborator-image"
-              alt="Mauro Gonzalez"
-            />
-            <h5 className="collaborator-name">Mauro Gonzalez</h5>
-            <FaLinkedin />
-          </div>
-        </div>
-        <div className="card">
-          <h2>Colaborador destacado</h2>
-          <div className="collaborator-card">
-            <img
-              src="https://i.pinimg.com/originals/01/7e/6f/017e6f2d0c32627056d26a7e77c75753.png"
-              className="collaborator-image"
-              alt="Ignacio Armendariz"
-            />
-            <h5 className="collaborator-name">Ignacio Armendariz</h5>
-
-            <FaLinkedin />
-          </div>
-        </div>
-        <div className="card">
-          <h2>Colaborador destacado</h2>
-          <div className="collaborator-card">
-            <img
-              src="https://i.pinimg.com/originals/01/7e/6f/017e6f2d0c32627056d26a7e77c75753.png"
-              className="collaborator-image"
-              alt="Santiago Buchhammer"
-            />
-            <h5 className="collaborator-name">Santiago Buchhammer</h5>
-            <FaLinkedin />
-          </div>
-        </div>
-        <div className="card">
-          <h2>Colaborador destacado</h2>
-          <div className="collaborator-card">
-            <img
-              src="https://i.pinimg.com/originals/01/7e/6f/017e6f2d0c32627056d26a7e77c75753.png"
-              className="collaborator-image"
-              alt="Mateo Barreiro"
-            />
-            <h5 className="collaborator-name">Mateo Barreiro</h5>
-            <FaLinkedin />
-          </div>
-        </div>
-      </div> */}
     </div>
   )
 }
--- src/utils/randomBanner.js
+++ src/utils/randomBanner.js
@@ -1,6 +1,6 @@
 const randomBanners = () => {
   const banners = [
-"https://lgecine.org/wp-content/uploads/2010/11/el-padrino_banner.png"  ,  "https://mir-s3-cdn-cf.behance.net/project_modules/1400/d2ac1f78219513.5c9e43ebb76f9.png",
+"https://timelinecovers.pro/facebook-cover/download/the-godfather5-facebook-cover.jpg"  ,  "https://mir-s3-cdn-cf.behance.net/project_modules/1400/d2ac1f78219513.5c9e43ebb76f9.png",
     "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/69f90eed-5acf-4c96-b8eb-47e2795ff495/da4221s-5a92ee59-23c7-4fb3-88cc-bc3bfa8a8a78.jpg/v1/fill/w_1024,h_342,q_75,strp/joker_banner_by_remixsdesigns_da4221s-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MzQyIiwicGF0aCI6IlwvZlwvNjlmOTBlZWQtNWFjZi00Yzk2LWI4ZWItNDdlMjc5NWZmNDk1XC9kYTQyMjFzLTVhOTJlZTU5LTIzYzctNGZiMy04OGNjLWJjM2JmYThhOGE3OC5qcGciLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.KWciDQBMpytXUhonaYISos765WfoUXIOiDOkBKuYiHo",
   ]
   return banners[Math.floor(Math.random() * banners.length)]
